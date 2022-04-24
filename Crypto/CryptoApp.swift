e8c4df9dd8d9390da527431a9d23790aed1996e5
iudexgundir <govoroverx@icloud.com>
2022-04-24 14:49:32 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Эрхаан Говоров on 24.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
